b3613636e26ce311728d6641f8ec3e1bc527790b
rodolfo <rodolfo.oliveira.106@ufrn.edu.br>
2021-07-20 04:42:05 -0300

    fix: corrects for CI build

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:js';
-
 import 'package:bank_system/pages/create_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
--- test/operations_test.dart
+++ test/operations_test.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:bank_system/back_end/models/account.dart';
 import 'package:bank_system/back_end/operations.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -9,7 +7,7 @@ void main() {
     Operation op = Operation();
     var created = op.createAccount(123);
     Account ac = Account(number: 123, currentValue: 0);
-    expect(created.currentValue, equals(0));
-    expect(created.number, equals(123));
+    expect(created.currentValue, equals(ac.currentValue));
+    expect(created.number, equals(ac.number));
   });
 }
