1ee3c97b0382551cca5edf59d1c35398b2060c7b
rodolfo <rodolfo.oliveira.106@ufrn.edu.br>
2021-07-20 00:04:15 -0300

    fix: add unit test for CI pass

---
+++ test/account_test.dart
@@ -0,0 +1,16 @@
+import 'package:bank_system/back_end/models/account.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  Account c;
+  test("class Account", () {
+    c = Account(number: 300, currentValue: 200.1);
+
+    expect(c.number, equals(300));
+    expect(c.currentValue, equals(200.1));
+
+    c.currentValue += 100;
+
+    expect(c.currentValue, (300.1));
+  });
+}
