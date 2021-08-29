75b35dead641165b0c64faf5070012992558755e
Rodolfo Dantas de Oliveira Bezerra <47856330+denpufa@users.noreply.github.com>
2021-08-29 18:41:51 -0300

    fix:put the tests on correct test file (#29)
    * fix:put the tests on correct test file
    
    * fix: imports unused
    
    Co-authored-by: rodolfo <rodolfo.oliveira.106@ufrn.edu.br>

--- test/account_test.dart
+++ test/account_test.dart
@@ -1,4 +1,6 @@
 import 'package:bank_system/back_end/models/account.dart';
+import 'package:bank_system/back_end/models/bonus_account.dart';
+import 'package:bank_system/back_end/operations.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
@@ -8,5 +10,24 @@ void main() {
 
     expect(c.number, equals(300));
     expect(c.currentValue, equals(200.1));
+  });
+  test("create account simples", () {
+    Operation op = Operation();
+    var created = op.createAccount(123, 'Simples');
+    Account ac = Account(number: 123, currentValue: 0);
+    expect(created.currentValue, equals(ac.currentValue));
+    expect(created.number, equals(ac.number));
+    expect(created.runtimeType, equals(ac.runtimeType));
+  });
+
+  test("create account Bônus", () {
+    Operation op = Operation();
+    dynamic created = op.createAccount(123, 'Bônus');
+    BonusAccount ac = BonusAccount(
+      number: 123,
+    );
+    expect(created.currentValue, equals(ac.currentValue));
+    expect(created.number, equals(ac.number));
+    expect(created.runtimeType, equals(ac.runtimeType));
   });
 }
--- test/operations_test.dart
+++ test/operations_test.dart
@@ -1,30 +1,7 @@
-import 'package:bank_system/back_end/models/account.dart';
-import 'package:bank_system/back_end/models/bonus_account.dart';
 import 'package:bank_system/back_end/operations.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test("create account simples", () {
-    Operation op = Operation();
-    var created = op.createAccount(123, 'Simples');
-    Account ac = Account(number: 123, currentValue: 0);
-    expect(created.currentValue, equals(ac.currentValue));
-    expect(created.number, equals(ac.number));
-    expect(created.runtimeType, equals(ac.runtimeType));
-  });
-
-  test("create account Bônus", () {
-    Operation op = Operation();
-    dynamic created = op.createAccount(123, 'Bônus');
-    BonusAccount ac = BonusAccount(
-      number: 123,
-    );
-    expect(created.runtimeType, equals(ac.runtimeType));
-    expect(created.currentValue, equals(ac.currentValue));
-    expect(created.number, equals(ac.number));
-    expect(created.cumulativePoints, equals(ac.cumulativePoints));
-  });
-
   test("consult balance", () {
     Operation op = Operation();
     var created = op.createAccount(123, 'Poupança');
