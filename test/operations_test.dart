027c7d8f11ab79049d89a842c4ed4876f0431863
Rodolfo Dantas de Oliveira Bezerra <47856330+denpufa@users.noreply.github.com>
2021-08-29 17:09:21 -0300

    new: tests for bonus account and correct for simple (#28)
    Co-authored-by: rodolfo <rodolfo.oliveira.106@ufrn.edu.br>

--- test/operations_test.dart
+++ test/operations_test.dart
@@ -1,33 +1,30 @@
 import 'package:bank_system/back_end/models/account.dart';
+import 'package:bank_system/back_end/models/bonus_account.dart';
 import 'package:bank_system/back_end/operations.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test("create account", () {
+  test("create account simples", () {
     Operation op = Operation();
     var created = op.createAccount(123, 'Simples');
     Account ac = Account(number: 123, currentValue: 0);
     expect(created.currentValue, equals(ac.currentValue));
     expect(created.number, equals(ac.number));
+    expect(created.runtimeType, equals(ac.runtimeType));
   });
 
-  ///this tests are intregate tests not units
-  /* 
-  test("transfer test", () {
+  test("create account Bônus", () {
     Operation op = Operation();
-    op.createAccount(10);
-    op.createAccount(3);
-    op.transfer(10, 3, 20);
-    expect(Data.accounts[0].currentValue, equals(-20));
-    expect(Data.accounts[1].currentValue, equals(20));
+    dynamic created = op.createAccount(123, 'Bônus');
+    BonusAccount ac = BonusAccount(
+      number: 123,
+    );
+    expect(created.runtimeType, equals(ac.runtimeType));
+    expect(created.currentValue, equals(ac.currentValue));
+    expect(created.number, equals(ac.number));
+    expect(created.cumulativePoints, equals(ac.cumulativePoints));
   });
 
-  test("debit test", () {
-    Operation op = Operation();
-    op.createAccount(10);
-    op.debit(10, 20);
-    expect(Data.accounts[0].currentValue, equals(-20));
-  }); */
   test("consult balance", () {
     Operation op = Operation();
     var created = op.createAccount(123, 'Poupança');
